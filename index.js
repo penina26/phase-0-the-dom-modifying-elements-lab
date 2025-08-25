ad9872068c99303a91c440852b6f227d6fe8bdcc
Penina <wpenina@gmail.com>
2025-08-25 18:16:41 +0300

    dom manipulation

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+// Find the <main> element with the id 'main'
+const mainElement = document.querySelector('#main');
+
+// Remove it from the document
+mainElement.remove();
+
+// Create a new <h1> element in memory
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> to 'victory'
+newHeader.id = 'victory';
+
+// Set its text content  "Penina" with your name
+newHeader.textContent = 'Penina is the champion';
+
+// Add the new <h1> to the end of the <body>
+document.body.append(newHeader);
